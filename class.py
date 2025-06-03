fcae401a34f20c2b198a49a5b42178f9ac27215d
Swarup Kadam <103502883+thesk26@users.noreply.github.com>
2025-06-03 03:47:24 +0000

    sdk

--- class.py
+++ class.py
@@ -996,79 +996,79 @@ print("List 3:", list_of_lists[2])
 # Tuples
 # A tuple is a collection which is ordered and unchangeable.
 
-# tup =()
-# print(tup)
-# print(type(tup))
+tup =()
+print(tup)
+print(type(tup))
 
-# tup =(2.9)
-# print(tup)
-# print(type(tup))
+tup =(2.9)
+print(tup)
+print(type(tup))
 
-# tup =("ashwin " )
-# print(tup)
-# print(type(tup))
+tup =("ashwin " )
+print(tup)
+print(type(tup))
 
 # if you want to create its type as a tuple then you have to add ,
 
-# tup =("ashwin" ,)
-# print(tup)
-# print(type(tup))
+tup =("ashwin" ,)
+print(tup)
+print(type(tup))
 
-# tup =( 1 , 2 , 3 , 4 ,5)
-# print(tup [1:3])
+tup =( 1 , 2 , 3 , 4 ,5)
+print(tup [1:3])
 
-# thistuple = ("apple", "banana", "cherry")
-# print(thistuple)
+thistuple = ("apple", "banana", "cherry")
+print(thistuple)
 
-# thistuple = tuple(("apple", "banana", "cherry"))
-# print(len(thistuple))
+thistuple = tuple(("apple", "banana", "cherry"))
+print(len(thistuple))
 
-# thistuple = ("apple", "banana", "cherry")
-# print(thistuple[1])
+thistuple = ("apple", "banana", "cherry")
+print(thistuple[1])
 
-# thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
-# print(thistuple[-4:-1])
+thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(thistuple[-4:-1])
 
 # update  tuple 
 
-# x = ("apple", "banana", "cherry")
+x = ("apple", "banana", "cherry")
 
-# y = list(x)
+y = list(x)
 
-# y[1] = "kiwi"
-# x = tuple(y)
+y[1] = "kiwi"
+x = tuple(y)
 
-# print(x)
-# print(type(x ,))
+print(x)
+print(type(x ,))
 
-# thistuple = ("apple", "banana", "cherry")
-# for x in thistuple:
-#     print(x)
+thistuple = ("apple", "banana", "cherry")
+for x in thistuple:
+    print(x)
 
-# tuple1 = ("a", "b" , "c")
-# tuple2 = (1, 2, 3)
+tuple1 = ("a", "b" , "c")
+tuple2 = (1, 2, 3)
 
-# tuple3 = tuple1 + tuple2
-# print(tuple3)
+tuple3 = tuple1 + tuple2
+print(tuple3)
 
-# print(type(tuple3 ,))
+print(type(tuple3 ,))
 
-# fruits = ("apple", "banana", "cherry")
-# mytuple = fruits * 2
+fruits = ("apple", "banana", "cherry")
+mytuple = fruits * 2
 
-# print(mytuple)
+print(mytuple)
 
 # methods in tuples 
 
 # index
 
-# tup = ( 1 ,2 ,3 , 4)
-# print(tup.index(4))
+tup = ( 1 ,2 ,3 , 4)
+print(tup.index(4))
 
 # ans is 1 because we call it index of value 2 in tuple
 
-# tup = ( 1 ,2 ,3 , 4 , 2 , 2)
-# print(tup.index(2))
+tup = ( 1 ,2 ,3 , 4 , 2 , 2)
+print(tup.index(2))
 
 # This method searches the tuple for the first occurrence of the value 2 and returns its index.
 
@@ -1078,124 +1078,83 @@ print("List 3:", list_of_lists[2])
 
 # count
 
-# tup = ( 1 ,2 ,3 , 4 , 2 , 2 , 2 ,2)
-# print(tup.count(3))
+tup = ( 1 ,2 ,3 , 4 , 2 , 2 , 2 ,2)
+print(tup.count(3))
 
+################################################################################################################
 # Day 14 of learning python
 # 26/05/2025  
 # class 14
 
 # QUESTION ANSWER ON tuple
-
-
 # 1 create a tuple 
-
 # Creating a tuple
-
-
-# my_tuple = (10, 20, 30, 40)
-
-# print("Tuple:", my_tuple)
-
-
-
-
-
+my_tuple = (10, 20, 30, 40)
+print("Tuple:", my_tuple)
 
 # 2.0 add the integer in tuple 
-
-# numbers = (34,54,67,21,78,97,45,44,80,19)
-# total = 0
-# for num in numbers:
-#    total += num
-# print("Total =", total)
-
+numbers = (34,54,67,21,78,97,45,44,80,19)
+total = 0
+for num in numbers:
+   total += num
+print("Total =", total)
 
 # 2 Write a Python program to create a tuple with different data types
-
-# tuplex = ("tuple", False, 3.2, 1)
-
-# print(tuplex)
-
-
+tuplex = ("tuple", False, 3.2, 1)
+print(tuplex)
 
 # 3 Write a Python program to create a tuple of numbers and print one item.
 
-# tuplex = 5, 10, 15, 20, 25
-
-# print(tuplex)
-
-# tuplex = 5,
-
-# print(tuplex)
-
-
+tuplex = 5, 10, 15, 20, 25
+print(tuplex)
+tuplex = 5,
+print(tuplex)
 
 # 4 Write a Python program to unpack a tuple into several variables.
 
-
-
-# tuplex = 4, 8, 3
-
-# print(tuplex)
-
-# n1, n2, n3 = tuplex
-
-# print(n1 + n2 + n3)
-
-
+tuplex = 4, 8, 3
+print(tuplex)
+n1, n2, n3 = tuplex
+print(n1 + n2 + n3)
 
 # 5 Write a Python program to add an item to a tuple
 
+tuplex = (4, 6, 2, 8, 3, 1)
+print(tuplex)
 
+tuplex = tuplex + (9,)
 
-# tuplex = (4, 6, 2, 8, 3, 1)
-# print(tuplex)
+print(tuplex)
 
-# tuplex = tuplex + (9,)
+listx = list(tuplex)
 
-# print(tuplex)
+listx.append(30)
 
+tuplex = tuple(listx)
 
-
-# listx = list(tuplex)
-
-# listx.append(30)
-
-# tuplex = tuple(listx)
-
-# print(tuplex) 
-
-
+print(tuplex) 
 
 # 6 Write a Python program to get the 4th element from the last element of a tuple.
 
+tuplex = ("w", 3, "r", "e", "s", "o", "u", "r", "c", "e")
+print(tuplex)
 
+# Get the 4th element of the tuple (index 3)
+item = tuplex[3]
+print(item)
 
-# tuplex = ("w", 3, "r", "e", "s", "o", "u", "r", "c", "e")
-
-# print(tuplex)
-
-# # Get the 4th element of the tuple (index 3)
-# item = tuplex[3]
-# print(item)
-
-# # Get the 4th element from the end of the tuple (index -4)
-# item1 = tuplex[-4]
-# print(item1) 
-
-
+# Get the 4th element from the end of the tuple (index -4)
+item1 = tuplex[-4]
+print(item1) 
 
 # 7Write a Python program to remove an item from a tuple.
 
+tuplex = "w", 3, "r", "s", "o", "u", "r", "c", "e"
+print(tuplex)
 
+listx = list(tuplex)
 
-# tuplex = "w", 3, "r", "s", "o", "u", "r", "c", "e"
-# print(tuplex)
+listx.remove("c")
+tuplex = tuple(listx)
 
-# listx = list(tuplex)
-
-# listx.remove("c")
-# tuplex = tuple(listx)
-
-# print(tuplex)
+print(tuplex)
